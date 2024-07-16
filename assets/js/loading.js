c59bc3e0d96c7bd9beb5fbd0d0259abe6d93c973
rodriimenezes <rodriisabatico@gmail.com>
2024-07-16 17:47:53 -0300

    added favicon

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,7 +1,7 @@
-function myFunc(){
-    myvar = setTimeout(myFunc2,1000)
+function loading(){
+    myvar = setTimeout(showPage,1000)
 }
-function myFunc2(){
+function showPage(){
     document.getElementById("load").style.display = "none"
     document.getElementById("page").style.display = "flex"
 }
